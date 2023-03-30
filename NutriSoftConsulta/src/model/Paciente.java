c900563a0a2547e8a4b3abbcb01b4e80339566e0
Jonathan Araujo <jonathan.adao@outlook.com>
2023-03-29 23:49:58 -0300

    CommitNutri
    Commit final, sistema pronto.

---
+++ NutriSoftConsulta/src/model/Paciente.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Paciente {
+
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private String data;
+    private String éPaciente;
+    private String ConsultaRealizada;
+    private String Descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getéPaciente() {
+        return éPaciente;
+    }
+
+    public void setéPaciente(String éPaciente) {
+        this.éPaciente = éPaciente;
+    }
+
+    public String getConsultaRealizada() {
+        return ConsultaRealizada;
+    }
+
+    public void setConsultaRealizada(String ConsultaRealizada) {
+        this.ConsultaRealizada = ConsultaRealizada;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    
+    
+
+}
